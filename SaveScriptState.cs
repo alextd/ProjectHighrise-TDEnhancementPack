dfea38898a5f2a9f2e836e016bf4b1317a259771
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2021-05-06 15:23:47 -0700

    So, saving/loading the game caused every NPC to forget what they're doing... sometimes, maybe.
    This is halfway fixed by setting their Unity GameObjects to inactive while loading, and setting active once loading is done. They were before executing their scripts before the game data was loaded, thought they weren't inside the building, and decided they couldn't work.
    
    Now, though, they do continue their assigned work. But the work progress is not saved. This may not be fixable as it is the time to completion is only saved in the Script Component object, not the Entity Data which is what gets saved. But it is not as bad as workers going back to their office before going all the way back up to work.
    
    Also included with this update is a log of debug spam while I work on this.

---
+++ SaveScriptState.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using HarmonyLib;
+using Game.Session.Entities.Components;
+using Game.Session.Entities;
+using Game.Session.Entities.Data;
+using Game.Session.Board;
+using Game.Components;
+using SomaSim.AI;
+using Game.AI;
+
+namespace BetterPlacement
+{
+	[HarmonyPatch(typeof(PeepComponent), nameof(PeepComponent.PickFromSchedule))]
+	public static class SaveScriptState
+	{
+		public static void LogScriptQueue(Entity e)
+		{
+			Log.Debug($"Queue for {e}:{e.id}");
+			if (e.components?.script?.queue is GameScriptQueue q && !q.IsEmpty)
+			{
+				foreach (Script s in q)
+					Log.Debug($"- {s.Name}");
+			}
+			else Log.Debug("- (empty)");
+		}
+		//PickFromSchedule
+		public static void Prefix(PeepComponent __instance, Entity ____entity)
+		{
+			Log.Debug($"Scheduling {____entity}:{____entity.id}");
+			LogScriptQueue(____entity);
+		}
+		public static void Postfix(PeepComponent __instance, Entity ____entity)
+		{
+			Log.Debug($"Scheduled {____entity}:{____entity.id}");
+			LogScriptQueue(____entity);
+		}
+	}
+
+
+	[HarmonyPatch(typeof(ScheduleManager), nameof(ScheduleManager.InBuilding))]
+	public static class LogInBuilding
+	{
+		//private static bool InBuilding(Entity entity)
+		public static void Postfix(bool __result, Entity entity)
+		{
+			Log.Debug($"InBuilding({entity}:{entity.id}) -> {__result}");
+			Log.Debug($"entity.components.sprite.IsVisible ({entity.components.sprite.IsVisible})");
+			Log.Debug($"Game.ctx.board.grid.FindGridCellOrNull(entity) ({Game.Game.ctx.board.grid.FindGridCellOrNull(entity)})");
+			Log.Debug($"Game.ctx.board.grid.FindGridCellOrNull(entity).hasFloor ({Game.Game.ctx.board.grid.FindGridCellOrNull(entity).hasFloor})");
+		}
+	}
+
+	//private void InitNewGameBoard(GameEvent gev)
+	[HarmonyPatch(typeof(StartupLevelGenerator), nameof(StartupLevelGenerator.InitNewGameBoard))]
+	public static class TESTLOG
+	{
+		public static void Prefix()
+		{
+			Log.Debug(":::INITNEWGAMEBOARD");
+		}
+	}
+	//internal void AddFloor(FloorType ft = FloorType.Default)
+	[HarmonyPatch(typeof(GridCell), nameof(GridCell.AddFloor))]
+	public static class TESTLOG2
+	{
+		public static void Prefix(GridCell __instance)
+		{
+			Log.Debug($":::AddFloor({__instance}");
+		}
+	}
+
+	//Does Loaded peeps update before Grid is loaded?
+	[HarmonyPatch(typeof(PeepComponent), nameof(PeepComponent.Update))]
+	public static class TESTLOG3
+	{
+		public static void Prefix(Entity ____entity)
+		{
+			Log.Debug($"Update({____entity}:{____entity.id})");
+		}
+	}
+
+	//public void Load(Hashtable data)
+	[HarmonyPatch(typeof(Grid), nameof(Grid.Load))]
+	public static class TESTLOG4
+	{
+		public static void Prefix()
+		{
+			Log.Debug($":::GRID LOADING");
+		}
+	}
+
+	[HarmonyPatch(typeof(EntityManager), nameof(EntityManager.CreateByTemplate))]
+	public static class DeactivateWhileLoading
+	{
+	//public Entity CreateByTemplate(EntityTemplate configs, int id = 0, EntityData savedata = null)
+		public static void Postfix(Entity __result, EntityData savedata)
+		{
+			if (savedata != null)  //Loaded
+				__result.go?.SetActive(false);
+		}
+	}
+
+	[HarmonyPatch(typeof(EntityManager), nameof(EntityManager.OnAfterLoad))]
+	public static class ActivateAfterLoad
+	{
+	//public void OnAfterLoad()
+		public static void Postfix(EntityManager __instance)
+		{
+			foreach (Entity e in __instance.GetAllEntities())
+				e.go?.SetActive(true);
+		}
+	}
+}
