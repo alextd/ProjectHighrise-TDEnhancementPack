12df53eb2d5a976ffec3365de74c9a61e2a68a49
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2021-05-14 10:50:15 -0700

    Fix so the, uh, zooming to mouse code doesn't... make the new room tool cursor invisible? It accidentally set the z transform to 0. So that would do it.

--- ZoomToMouse.cs
+++ ZoomToMouse.cs
@@ -11,33 +11,32 @@ using UnityEngine;
 
 namespace BetterPlacement
 {
+	//------------------
+	//Stack zoom requests
+	//------------------
 	[HarmonyPatch(typeof(BaseInputMode), nameof(BaseInputMode.OnZoom))]
 	public static class StackZoomWheel
 	{
 		//public virtual bool OnZoom(Vector2? pos, float zoomfactor, bool tween)
-		public static bool Prefix(ref bool __result, BaseInputMode __instance, Vector2? pos, float zoomfactor, bool tween)
+		public static bool Prefix(ref bool __result, BaseInputMode __instance, float zoomfactor, bool tween)
 		{
 			if (Game.Util.UIUtil.IsInputOverUI)
 			{
 				__result = false;
+				return false;
 			}
-			else if (pos.HasValue)
-			{
-				zoomfactor /= 3;//Honestly it's zooming too fast.
+			zoomfactor /= 3;//Honestly it's zooming too fast.
 
-				//float zoom = Game.Game.serv.camera.GetZoom();
-				//This is the current zoom, but where is it currently zooming to?
+			//float zoom = Game.Game.serv.camera.GetZoom();
+			//This is the current zoom, but where is it currently zooming to?
 
-				float zoom = DesiredZoom.desiredZoom;
+			float zoom = DesiredZoom.desiredZoom;
 
-				float zoomFactor = 1f + Mathf.Abs(zoomfactor * __instance.ZoomMultiplier);
-				zoom = zoomfactor >= 0f ? (zoom / zoomFactor) : (zoom * zoomFactor);
-				__instance._isDown = false;
-				Game.Game.serv.camera.SetZoom(zoom, tween, 0.15f);
-				__result = true;
-			}
-			else
-				return true;
+			float zoomFactor = 1f + Mathf.Abs(zoomfactor * __instance.ZoomMultiplier);
+			zoom = zoomfactor >= 0f ? (zoom / zoomFactor) : (zoom * zoomFactor);
+			__instance._isDown = false;
+			Game.Game.serv.camera.SetZoom(zoom, tween, 0.15f);
+			__result = true;
 
 			return false;
 		}
@@ -54,6 +53,9 @@ namespace BetterPlacement
 		}
 	}
 
+	//------------------
+	//Zoom to mouse
+	//------------------
 	[HarmonyPatch(typeof(CameraService), nameof(CameraService.CameraZoomHelper))]
 	public static class ZoomToMouse
 	{
@@ -62,27 +64,30 @@ namespace BetterPlacement
 		{
 			//Get where the mouse is
 			Vector3 mousePosition = UnityEngine.Input.mousePosition;
-			WorldPos oldpos = Game.Game.serv.camera.ScreenToWorldPos(mousePosition);
-			
-			//Old Zoom code
+			WorldPos oldpos = __instance.ScreenToWorldPos(mousePosition);
+
+			//Apply zoom
 			val = __instance.ClampZoom(val);
 			__instance.main.orthographicSize = val;
-			Transform transform = __instance.main.transform;
-			Vector2 transformPos = __instance.ClampPos(transform.position, val);
-			float x = transformPos.x;
-			float y = transformPos.y;
-			Vector3 newTransform = new Vector3(transformPos.x, transformPos.y, transform.position.z);
-			transform.position = newTransform;
-			//----
 
 			//Find out where we are now:
-			WorldPos newpos = Game.Game.serv.camera.ScreenToWorldPos(mousePosition);
+			WorldPos newpos = __instance.ScreenToWorldPos(mousePosition);
 
 			//Find how much we slide away from cursor and adjust:
-			newTransform.x += oldpos.x - newpos.x; newTransform.y += oldpos.y - newpos.y;
-			transform.position = __instance.ClampPos(newTransform, val);
+			Vector3 newTransform = new Vector3(
+				__instance.main.transform.position.x + oldpos.x - newpos.x,
+				__instance.main.transform.position.y + oldpos.y - newpos.y,
+				__instance.main.transform.position.z);
 
-			Game.Game.serv.camera.OnCameraZoomChanged.Invoke();
+			//clamp panning position for out of bounds
+			Vector2 transformPos = __instance.ClampPos(newTransform, val);
+			//ClampPos takes Vector3 spits out a Vector 2 :/
+			newTransform.x = transformPos.x; newTransform.y = transformPos.y;
+
+			//And set it
+			__instance.main.transform.position = newTransform;
+
+			__instance.OnCameraZoomChanged.Invoke();
 
 			return false;
 		}
